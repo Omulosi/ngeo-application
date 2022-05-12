33567997e2b6cd54f680bcbd5edf8878ff5ba0d6
Omulosi <mulongojohnpaul@gmail.com>
2022-05-12 21:44:46 +0300

    Add data fetchers for deputy county manager

--- ngeo-ui/src/fetch/county_managers/index.js
+++ ngeo-ui/src/fetch/county_managers/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable function-paren-newline */
+/* eslint-disable implicit-arrow-linebreak */
 import { useQuery } from 'react-query';
 import fetchResource from '../fetchers';
 
@@ -11,8 +13,18 @@ const getCountyManagerById = async (id) => {
   return data;
 };
 
+const getDeputyCountyManagerById = async (id) => {
+  const { data } = await fetchResource(`/deputy_county_managers/${id}`);
+  return data;
+};
+
 const getCountyManagerList = async () => {
   const { data } = await fetchResource('/county_managers');
+  return data;
+};
+
+const getDeputyCountyManagerList = async () => {
+  const { data } = await fetchResource('/deputy_county_managers');
   return data;
 };
 
@@ -26,4 +38,16 @@ export function useCountyManagerById(cmId) {
 
 export function useCountyManagerList() {
   return useQuery('countyManager-list', () => getCountyManagerList());
+}
+
+export function useDeputyCountyManagerList() {
+  return useQuery('deputy-countyManager-list', () =>
+    getDeputyCountyManagerList()
+  );
+}
+
+export function useDeputyCountyManagerById(cmId) {
+  return useQuery(['deputy-countyManager', cmId], () =>
+    getDeputyCountyManagerById(cmId)
+  );
 }
