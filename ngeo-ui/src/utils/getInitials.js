ae0bd839dd0fbd299838fd5a1a221b3837820713
Omulosi <mulongojohnpaul@gmail.com>
2022-05-12 21:45:32 +0300

    Add comment to getInitial util function

--- ngeo-ui/src/utils/getInitials.js
+++ ngeo-ui/src/utils/getInitials.js
@@ -1,6 +1,11 @@
-export default (name = '') => name
-  .replace(/\s+/, ' ')
-  .split(' ')
-  .slice(0, 2)
-  .map((v) => v && v[0].toUpperCase())
-  .join('');
+/* eslint-disable implicit-arrow-linebreak */
+/**
+ * Returns names's initials
+ */
+export default (name = '') =>
+  name
+    .replace(/\s+/, ' ')
+    .split(' ')
+    .slice(0, 2)
+    .map((v) => v && v[0].toUpperCase())
+    .join('');
